3df6303d8c3f112cd724060f5acde9de1c430907
Aleks <lih2@yandex.ru>
2018-05-25 17:06:15 +0300

    Del congiguration

--- src/ru/ocrimea/simplecaradmin/CarStorage.java
+++ src/ru/ocrimea/simplecaradmin/CarStorage.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Component
 @Repository
+@Service
 
 public class CarStorage {
 
