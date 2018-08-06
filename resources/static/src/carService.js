a6c32fea5aaef9da2d8c08293877f6b173a42cd0
Aleks <lih2@yandex.ru>
2018-08-06 19:33:38 +0300

    CarList CarService Beta

---
+++ resources/static/src/carService.js
@@ -0,0 +1,56 @@
+export class CarService {
+
+
+    getCars() {
+        return new Promise( (resolve, reject) => {
+            $.ajax('/caradmin', {
+                type: 'GET',
+                contentType: "application/json",
+                dataType: "json",
+                success: resolve,
+                error: reject
+            })
+        });
+    }
+
+    addCar(number,name,year,price) {
+
+        let JSONObject= {
+            "name":name,
+            "number":number,
+            "price":price,
+            "year":year
+        };
+
+        return new Promise( (resolve, reject) => {
+            $.ajax('/caradd', {
+                type: 'POST',
+                data: JSON.stringify(JSONObject),
+                contentType: "application/json",
+                dataType: "json",
+                success: resolve,
+                error: reject
+            })
+        });
+    }
+
+    deleteCar(number) {
+
+        let JSONObject= {
+            "number":number,
+        };
+
+        return new Promise( (resolve, reject) => {
+            $.ajax('/cardel', {
+                type: 'POST',
+                data: JSON.stringify(JSONObject),
+                contentType: "application/json",
+                dataType: "json",
+                success: resolve,
+                error: reject
+            })
+        });
+    }
+
+
+}
--- resources/static/src/index.js
+++ resources/static/src/index.js
@@ -1,216 +1,163 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {CarService} from './carService';
 
-class CarApp extends React.Component {
-	
+class CarList extends React.Component {
+
     constructor(props) {
-          super(props);			  
-		  this.state = {
-	        cars: []        			
-            };	 
-          this.addCar = this.addCar.bind(this);				
-          this.deleteCar = this.deleteCar.bind(this);			  
-	}
-	
-	componentDidMount() {
-		this.loadData();
-	}  
-			 
-   loadData() {
-     fetch('/caradmin').then(results => { return results.json() }).then(data => {
-          this.setState({cars: data });
-     }).catch(() => {
-          alert('Ошибка!');
-     });
-   }
-       
-  deleteCar(number) {
-	     let JSONObject= {
-             "number":number,
-             };	   	 		 		 		 
-		 fetch("/cardel", {
-			method: "POST",		
-			body: JSON.stringify(JSONObject),			
-			headers: {
-			'Accept': 'application/json, text/plain, */*',
-			'Content-Type': 'application/json'
-			},						
-		}).then(results => { return results.json() }).then(data => {
-          this.setState({cars: data });
-     }).catch(() => {
-          alert('Ошибка выполнения запроса!');
-     });	  
-  }
-    
-  addCar(number,name,year,price) {
-	     let JSONObject= {
-             "name":name,
-             "number":number,
-             "price":price,
-             "year":year
-             };	   	 		 		 		 
-		 fetch("/caradd", {
-			method: "POST",		
-			body: JSON.stringify(JSONObject),			
-			headers: {
-			'Accept': 'application/json, text/plain, */*',
-			'Content-Type': 'application/json'
-			},						
-		}).then(results => { return results.json() }).then(data => {
-          this.setState({cars: data });
-     }).catch(() => {
-          alert('Ошибка выполнения запроса!');
-     });
-			
-    }
-       		
-   render() {
-		return <div>
-		         <UserForm handleAddCar={this.addCar} />
-				 <CarData cars={this.state.cars} handleDeleteCar={this.deleteCar} />
-			   </div>;
-	}
-	
-}
-
-
-class UserForm extends React.Component {
-
-     constructor(props) {
-          super(props);
+		
+        super(props);
+		this.state = {
+		      cars: [],
+              number: '',
+              name: '',
+              year: '',
+              price: ''
+         };
+		 
+         this.carService = new CarService();		  
 		  
-		  this.state = {
-            number: '',
-            name: '',
-            year: '',
-            price: ''				
-            };	 
-			
-          this.handleChangeNumber = this.handleChangeNumber.bind(this);			  	
-		  this.handleChangeName = this.handleChangeName.bind(this);			  	
-		  this.handleChangeYear = this.handleChangeYear.bind(this);			  			  
-		  this.handleChangePrice = this.handleChangePrice.bind(this);			  			  		  
-				  
-          this.addCar = this.addCar.bind(this);	
-     }
-	 
-	handleChangeNumber(event) {
-        this.setState({
-            number: event.target.value,
-        });
-    }	 
-	handleChangeName(event) {
-        this.setState({
-            name: event.target.value,
-        });
-    }	 
-	handleChangeYear(event) {
-        this.setState({
-            year: event.target.value,
-        });
-    }	 
-	handleChangePrice(event) {
-        this.setState({
-            price: event.target.value,
-        });
-    }		 
-	 	 
-	addCar(e) {   
-      e.preventDefault();	
-	  this.props.handleAddCar(this.state.number,this.state.name,this.state.year,this.state.price);			 
-    }		 
-	
-   render() {	   
-	   return <form onSubmit={this.addCar}>
-
-        <div class="row">
-            <div class="col-xs-12 divfon">
-
-                <div class="form-group col-xs-2">
-                    <input onChange={this.handleChangeNumber} value={this.state.number} type="text" required class="form-control" name="number" id="number" placeholder="Номер машины" data-validation-required-message="Пожалуйста укажите номер машины" />
-                </div>
-
-                <div class="form-group col-xs-4">
-                    <input onChange={this.handleChangeName} value={this.state.name} type="text" required class="form-control" name="name" id="name" placeholder="Название машины" data-validation-required-message="Пожалуйста укажите марку машины и двигатель" />
-                </div>
-
-                <div class="form-group col-xs-3">
-                    <input onChange={this.handleChangeYear} value={this.state.year} type="number" required class="form-control" name="year" id="year" placeholder="Год выпуска" pattern="[0-9]{4}" data-validation-required-message="Пожалуйста укажите год выпуска, 4 цифры" />
-                </div>
-
-                <div class="form-group col-xs-3">
-                    <input onChange={this.handleChangePrice} value={this.state.price} type="number" required class="form-control" name="price" id="price" placeholder="Стоимость" pattern="[0-9]{1,}" data-validation-required-message="Пожалуйста укажите стоимость, только цифры" />
-                </div>
-
-                <div class="form-group col-xs-offset-9 col-xs-6">
-                    <button class="btn btn-default" name="add" id="add">Все верно, отправить</button>
-                </div>
-
-                <input type="hidden" name="action" value="add" />
-
-                <div class="clearfix"></div>
-
-
-            </div>
-        </div>
-    </form> ;
-	
-   }		
-}
-
-class CarData extends React.Component {
-
-     constructor(props) {	
-	  super(props);	
-	  
-      this.deleteCar = this.deleteCar.bind(this);		  
-    }
-	
-	deleteCar(number) {   
-	  this.props.handleDeleteCar(number);			 		
+		 this.handleChange = name => event => {
+			this.setState({ [name]: event.target.value });
+		  }
+		  
+		 this.handleDelete = event => {
+			this.deleteCar(event.target.id);
+		  }
+		  
+		 this.handleSubmit = event => {
+			this.addCar(event);
+		 }		  		  
 	}
 		
-  render() {
-        return (
-            <table className="table" >
-                <thead>
-                    <tr>
-                        <th>
-                          Номер машины
-                    </th>
-                    <th>
-                       Название
-                   </th>
-                    <th>
-                       Год
-                    </th>
-                    <th>
-                       Стоимость
-                    </th>
-                    <th>
-                    </th>
-                </tr>
-        </thead>
+	componentDidMount() {
+		this.loadData();
+	}
+	
+    loadData() {
+       this.carService.getCars().then(
+           (data) =>
+           {
+               this.setState({cars: data });
+           })
+           .catch((error) =>
+           {
+               alert("Ошибка загрузки данных");
+               console.log("Ошибка загрузки данных");
+           });
+   }
+   
+   addCar(e) {  
+	  e.preventDefault();	      
+      this.carService.addCar(this.state.number,this.state.name,this.state.year,this.state.price).then(
+           (data) =>
+           {
+               this.setState({cars: data });
+           })
+           .catch((error) =>
+           {
+               alert("Ошибка добавления автомобиля");
+               console.log("Ошибка добавления автомобиля");
+           });	 	   
+    }	
+	
+    deleteCar(number) {
+		
+      this.carService.deleteCar(number).then(
+           (data) =>
+           {
+               this.setState({cars: data });
+           })
+           .catch((error) =>
+           {
+               alert("Ошибка удаления автомобиля");
+               console.log("Ошибка удаления автомобиля");
+           });	 
+	}	   
+	   	  
+    render() {
+        return (<div> <form onSubmit={this.handleSubmit}>
+            <div className="row">
+                <div className="col-xs-12 divfon">
+
+                    <div className="form-group col-xs-2">
+                        <input onChange={this.handleChange('number')} value={this.state.number} type="text" required
+                               className="form-control" name="number" id="number" placeholder="Номер машины"
+                               data-validation-required-message="Пожалуйста укажите номер машины"/>
+                    </div>
+
+                    <div className="form-group col-xs-4">
+                        <input onChange={this.handleChange('name')} value={this.state.name} type="text" required
+                               className="form-control" name="name" id="name" placeholder="Название машины"
+                               data-validation-required-message="Пожалуйста укажите марку машины и двигатель"/>
+                    </div>
+
+                    <div className="form-group col-xs-3">
+                        <input onChange={this.handleChange('year')} value={this.state.year} type="number" required
+                               className="form-control" name="year" id="year" placeholder="Год выпуска"
+                               pattern="[0-9]{4}"
+                               data-validation-required-message="Пожалуйста укажите год выпуска, 4 цифры"/>
+                    </div>
+
+                    <div className="form-group col-xs-3">
+                        <input onChange={this.handleChange('price')} value={this.state.price} type="number" required
+                               className="form-control" name="price" id="price" placeholder="Стоимость"
+                               pattern="[0-9]{1,}"
+                               data-validation-required-message="Пожалуйста укажите стоимость, только цифры"/>
+                    </div>
+
+                    <div className="form-group col-xs-offset-9 col-xs-6">
+                        <button className="btn btn-default" name="add" id="add">Все верно, отправить</button>
+                    </div>
+
+                    <input type="hidden" name="action" value="add"/>
+
+                    <div className="clearfix"></div>
+
+                </div>
+            </div>
+        </form>
+
+        <table className="table">
+          <thead >
+           <tr>
+             <th>
+				Номер машины
+			</th>
+			<th>
+				Название
+			</th>
+			<th>
+				Год
+			</th>
+			<th>
+				Стоимость
+			</th>
+			<th>
+			</th>
+		</tr>
+		</thead>
+		
         <tbody>
-            {this.props.cars.map(item => {
-                return (
-                    <tr key={item.number}>
-                        <td>{item.number}</td>
-                        <td>{item.name}</td>
-                        <td>{item.year}</td>
-                        <td>{item.price}</td>                       
-                        <td><button class="btn btn-default" onClick={this.deleteCar.bind(null,item.number)}>Удалить</button></td>                       						
-                        </tr>
-                    );
-                })
-            }
+        {this.state.cars.map(item => {
+            return (
+                <tr key={item.number}>
+                    <td>{item.number}</td>
+                    <td>{item.name}</td>
+                    <td>{item.year}</td>
+                    <td>{item.price}</td>
+                    <td>
+                        <button className="btn btn-default" onClick={this.handleDelete} id={item.number}>Удалить
+                        </button>
+                    </td>
+                </tr>
+            );
+        })
+        }
         </tbody>
-    </table>);
-	}  
+        </table></div>) ;
+    }
 }
 
-
-//ReactDOM.render( <UserForm />, document.getElementById('userform'));
-//ReactDOM.render( <CarData />, document.getElementById('cardata'));
-ReactDOM.render( <CarApp />,document.getElementById('carapp'));
+ReactDOM.render( <CarList />,document.getElementById('carlist'));
 
