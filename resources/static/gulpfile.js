8926c876be03c1a57daa173c7c5481dac26b5e3a
Aleks <lih2@yandex.ru>
2018-08-08 22:31:09 +0300

    gulp-concat del

--- resources/static/gulpfile.js
+++ resources/static/gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var babel = require("gulp-babel");
-//var concat = require('gulp-concat');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var del = require('del');
